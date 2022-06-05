4f56e90c0b0b63408a5e3695fecd3133ceb64ef0
tboonma <tboonma@hotmail.com>
2022-06-05 23:13:45 +0700

    Fetch data from API

--- src/components/AdviceBox.tsx
+++ src/components/AdviceBox.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+type AdviceInfo = {
+    id: number,
+    description: string
+}
 
-const AdviceBox = () => {
+const AdviceBox = (adviceInfo: AdviceInfo) => {
   return (
-    <div>AdviceBox</div>
+    <div className="w-1/3 bg-dark-grayish-blue rounded-xl px-10 py-8 grid place-items-center gap-2">
+        <h1 className="text-neon-green">Advice #{adviceInfo.id}</h1>
+        <p className="text-center text-light-cyan">"{adviceInfo.description}"</p>
+    </div>
   )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../assets/css/styles.css'
 import AdviceBox from '../components/AdviceBox'
+import axios from 'axios'
 
 const Home = () => {
+    const [adviceId, setAdviceId] = useState(0)
+    const [adviceQuote, setAdviceQuote] = useState('')
+
+    useEffect(() => {
+        getAdvice()
+    }, [])
+
+    function getAdvice() {
+        axios.get('https://api.adviceslip.com/advice')
+            .then(response => {
+                setAdviceId(response.data.slip.id)
+                setAdviceQuote(response.data.slip.advice)
+            })
+            .catch((error) => {
+                alert("API error: " + error)
+                window.location.reload()
+            })
+    }
   return (
     <div className="grid place-items-center h-screen">
-        <AdviceBox />
+        <AdviceBox id={adviceId} description={adviceQuote} />
     </div>
   )
 }
