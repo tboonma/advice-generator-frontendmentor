f1476c1f3b1bf10368fa8be42841a67db9b2f688
tboonma <tboonma@hotmail.com>
2022-06-05 23:15:52 +0700

    Bigger font for quote

--- src/components/AdviceBox.tsx
+++ src/components/AdviceBox.tsx
@@ -7,7 +7,7 @@ const AdviceBox = (adviceInfo: AdviceInfo) => {
   return (
     <div className="w-1/3 bg-dark-grayish-blue rounded-xl px-10 py-8 grid place-items-center gap-2">
         <h1 className="text-neon-green">Advice #{adviceInfo.id}</h1>
-        <p className="text-center text-light-cyan">"{adviceInfo.description}"</p>
+        <p className="text-center text-light-cyan advice-quote">"{adviceInfo.description}"</p>
     </div>
   )
 }
