fe8d08bc0c5400b2cca5a620422b36e1a6485e21
tboonma <tboonma@hotmail.com>
2022-06-05 18:47:39 +0700

    Add pages and components

---
+++ src/components/AdviceBox.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdviceBox = () => {
+  return (
+    <div>AdviceBox</div>
+  )
+}
+
+export default AdviceBox
---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../assets/css/styles.css'
+import AdviceBox from '../components/AdviceBox'
+
+const Home = () => {
+  return (
+    <div className="grid place-items-center h-screen">
+        <AdviceBox />
+    </div>
+  )
+}
+
+export default Home
