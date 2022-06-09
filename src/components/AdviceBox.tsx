8e4712cd36ed159176bfadee3a77808078421610
tboonma <tboonma@hotmail.com>
2022-06-09 22:36:31 +0700

    Fix bugs in displaying in some browser

--- src/components/AdviceBox.tsx
+++ src/components/AdviceBox.tsx
@@ -9,14 +9,16 @@ type AdviceInfo = {
 
 const AdviceBox = (adviceInfo: AdviceInfo) => {
   return (
-    <div className="w-11/12 md:w-[37vw] bg-dark-grayish-blue rounded-xl px-10 pt-8 pb-16 grid place-items-center gap-4 absolute">
-        <p className="text-neon-green font-extrabold tracking-[0.3em] text-sm">ADVICE #{adviceInfo.id}</p>
-        <p className="text-center text-light-cyan advice-quote font-extrabold">"{adviceInfo.description}"</p>
-        <span className="text-center">
-          <img className="pattern-divider" src={PatternDivider} alt="Pattern Divider" />
-        </span>
-      <div className="absolute h-16 w-16 rounded-full bg-neon-green random-btn grid place-items-center transition duration-300 ease-in-out" onClick={adviceInfo.getAdvice}>
-        <img src={dice} alt="Dice icon" width="22px" height="auto" />
+    <div className="grid advice-section">
+      <div className="w-11/12 md:w-[37vw] bg-dark-grayish-blue rounded-xl px-10 pt-8 pb-16 grid place-items-center gap-4">
+          <p className="text-neon-green font-extrabold tracking-[0.3em] text-sm">ADVICE #{adviceInfo.id}</p>
+          <p className="text-center text-light-cyan advice-quote font-extrabold">"{adviceInfo.description}"</p>
+          <span className="text-center">
+            <img className="pattern-divider" src={PatternDivider} alt="Pattern Divider" />
+          </span>
+      </div>
+      <div className="h-16 w-16 rounded-full bg-neon-green random-btn grid place-items-center transition duration-300 ease-in-out" onClick={adviceInfo.getAdvice}>
+          <img src={dice} alt="Dice icon" width="22px" height="auto" />
       </div>
     </div>
   )
